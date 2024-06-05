a4b8a9aecd67b3cb33a5b8ba6e844daedd4ac23f
dachidadiani <dachidadiani.925@gmail.com>
2024-06-05 20:26:16 +0400

    second commit

---
+++ level 7/homework/homeworke.py
@@ -0,0 +1,46 @@
+name=input("enter your name:")
+last_name=input("enter your lasr name:")
+
+country=input("enter your country:")
+
+
+
+age=input("enter your age:")
+fav_color=input("enter your fav_color:")
+fav_car_brand=input("enter fav car brand:")
+fav_digit=input("enter fav_digit:")
+print(name ,  last_name , country , age , fav_color , age , fav_car_brand , fav_digit)
+
+
+
+
+
+print(7*7)
+print(12*12)
+print(15*15)
+
+print(7+7)
+print(12+12)
+print(15+15)
+
+print(7-7)
+print(12-12)
+print(15-15)
+
+print(7/7)
+print(12/12)
+print(15/15)
+
+print(7**7)
+print(12**12)
+print(15**15)
+
+
+
+
+
+
+
+
+
+
